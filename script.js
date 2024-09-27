48f9a9d945c18eba8d61f83930e9abef89637f3d
zay <tennn10@protonmail.com>
2024-09-27 07:10:05 -0500

    V1.3 Update

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-
 const clickbutton = document.getElementById("click");
 const bananas = document.getElementById("clicks");
 const upgradebutton = document.getElementById("upgrade");
 const towerbuttons = document.getElementsByClassName("tower");
 const sell = document.getElementById("sell");
 const money = document.getElementById("coins");
+const highscore = document.getElementById("highscore");
+const highscoreButton = document.getElementById("highscorebutton")
+
+const settingsMenu = document.getElementById("settingsmenu");
+const settingsButton = document.getElementById("settingsbutton");
+settingsMenu.style.visibility = 'hidden';
+
 let towers = [];
 let i = 0;
 let coins = 0;
 let upgrade = 1;
 let upgradecost = 50;
 
+let bananaHighScore = parseInt(localStorage.getItem('bananaHighScore')) || 0;
+let moneyHighScore = parseInt(localStorage.getItem('moneyHighScore')) || 0;
+highscore.innerText = `HIGHSCORE - bananas: ${bananaHighScore}, money: ${moneyHighScore}`;
+
+function updateHighScore() {
+  if (i > bananaHighScore) {
+    bananaHighScore = i;
+    localStorage.setItem('bananaHighScore', bananaHighScore);
+  }
+  if (coins > moneyHighScore) {
+    moneyHighScore = coins;
+    localStorage.setItem('moneyHighScore', moneyHighScore);
+  }
+  highscore.innerText = `HIGHSCORE - bananas: ${bananaHighScore}, money: ${moneyHighScore}`;
+}
+
 clickbutton.onclick = () => {
   i = i + upgrade;
   bananas.innerText = "bananas: " + i;
+  updateHighScore();
 };
 
 upgradebutton.onclick = () => {
@@ -24,6 +47,7 @@ upgradebutton.onclick = () => {
     bananas.innerText = "bananas: " + i;
     console.log("bananas: " + i);
     upgradebutton.innerText = "upgrade (" + upgradecost + " bananas)";
+    updateHighScore();
   } else {
     alert("You don't have enough bananas!");
   }
@@ -38,6 +62,7 @@ for (let c = 0; c < towerbuttons.length; c++) {
         i = i - 30;
         bananas.innerText = "bananas: " + i;
         console.log("miner added");
+        updateHighScore();
       }
     } else if (tempbutton.id == "stealer") {
       if (i >= 500) {
@@ -45,6 +70,7 @@ for (let c = 0; c < towerbuttons.length; c++) {
         i = i - 500;
         bananas.innerText = "bananas: " + i;
         console.log("stealer added");
+        updateHighScore();
       }
     } else if (tempbutton.id == "monkey") {
       if (coins >= 1000) {
@@ -52,6 +78,7 @@ for (let c = 0; c < towerbuttons.length; c++) {
         coins = coins - 1000;
         money.innerText = "money: " + coins;
         console.log("monkey added, coins left: " + coins);
+        updateHighScore();
       }
     } else if (tempbutton.id == "farmer") {
       if (i >= 200) {
@@ -59,6 +86,7 @@ for (let c = 0; c < towerbuttons.length; c++) {
         i = i - 200;
         bananas.innerText = "bananas: " + i;
         console.log("farmer added");
+        updateHighScore();
       }
     } else if (tempbutton.id == "collector") {
       if (coins >= 4500) {
@@ -66,6 +94,7 @@ for (let c = 0; c < towerbuttons.length; c++) {
         coins = coins - 4500;
         money.innerText = "money: " + coins;
         console.log("collector added, coins left: " + coins);
+        updateHighScore();
       }
     } else if (tempbutton.id == "marketer") {
       if (coins >= 8000) {
@@ -73,11 +102,11 @@ for (let c = 0; c < towerbuttons.length; c++) {
         coins = coins - 8000;
         money.innerText = "money: " + coins;
         console.log("marketer added, coins left: " + coins);
+        updateHighScore();
       }
     }
   };
 }
-
 
 setInterval(() => {
   for (let c = 0; c < towers.length; c++) {
@@ -103,23 +132,60 @@ setInterval(() => {
     if (towers[c] === "collector") {
       console.log("collector generating bananas");
       i = i + 350;
-      coins = coins + 700
+      coins = coins + 700;
       bananas.innerText = "bananas: " + i;
       money.innerText = "money: " + coins;
     }
     if (towers[c] === "marketer") {
       console.log("marketer generating bananas");
       i = i + 1350;
-      coins = coins + 1600
+      coins = coins + 1600;
       bananas.innerText = "bananas: " + i;
       money.innerText = "money: " + coins;
     }
   }
+  updateHighScore();
 }, 500);
 
 sell.onclick = () => {
+  updateHighScore();
   coins += i;
   i = 0;
   bananas.innerText = "bananas: " + i;
   money.innerText = "money: " + coins;
+};
+
+//settings stuff
+
+settingsButton.onclick = () => {
+  if (settingsMenu.style.visibility === 'hidden' || settingsMenu.style.visibility === '') {
+      settingsMenu.style.visibility = 'visible';
+      settingsMenu.style.opacity = 1;
+  } else {
+      settingsMenu.style.visibility = 'hidden';
+      settingsMenu.style.opacity = 0;
+  }
+};
+
+let highscoreEnabled = false;
+highscoreButton.onclick = () => {
+    highscoreEnabled = !highscoreEnabled;
+
+    if (highscoreEnabled) {
+        highscoreButton.textContent = "OFF";
+        highscore.style.display = "none";
+        enableHighscore();
+    } else {
+        highscoreButton.textContent = "ON";
+        highscore.style.display = "block";
+        disableHighscore();
+    }
+};
+
+function enableHighscore() {
+    console.log("Highscore enabled");
+}
+
+function disableHighscore() {
+    console.log("Highscore disabled");
 }
